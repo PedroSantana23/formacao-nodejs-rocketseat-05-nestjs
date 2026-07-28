facede919b6ce1e9eac97ea4721c1739f5cf5f5a
PedroSantana23 <ph.silva.santana@gmail.com>
2026-07-28 15:46:02 -0300

    2 - Desacoplando camadas no NestJS

---
+++ src/domain/forum/application/use-cases/register-student.spec.ts
@@ -0,0 +1,40 @@
+import { RegisterStudentUseCase } from './register-student'
+import { InMemoryStudentRepository } from 'test/repositories/in-memory-student-repository'
+import { FakeHasher } from 'test/cryptography/fake-hasher'
+
+let inMemoryStudentsRepository: InMemoryStudentRepository
+let fakeHasher: FakeHasher
+let sut: RegisterStudentUseCase
+
+describe('Register Student', () => {
+  beforeEach(() => {
+    inMemoryStudentsRepository = new InMemoryStudentRepository()
+    fakeHasher = new FakeHasher()
+
+    sut = new RegisterStudentUseCase(inMemoryStudentsRepository, fakeHasher)
+  })
+
+  it('should be able to register a new student', async () => {
+    const result = await sut.execute({
+      name: 'Fulano',
+      email: 'fulando@email.com',
+      password: '123456'
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(result.value).toEqual({
+      student: inMemoryStudentsRepository.items[0],
+    })
+  })
+
+  it('should hash student password upon registration', async () => {
+    const result = await sut.execute({
+      name: 'Fulano',
+      email: 'fulando@email.com',
+      password: '123456'
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryStudentsRepository.items[0].password).toEqual('123456-hashed')
+  })
+})
